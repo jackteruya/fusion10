40da4f17d897823c1d91e4b54e0ed9cc00d2eb41
jack.teruya <jack.teruya@gmail.com>
2022-03-09 14:36:03 -0400

    update

--- fusion10/settings.py
+++ fusion10/settings.py
@@ -17,7 +17,7 @@ import django_heroku
 from decouple import Csv, config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
